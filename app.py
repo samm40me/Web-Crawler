e0fde40fcd3ae70d51bb32e52637bc1f880f10b9
Samuel Ojagbohunmi <31414405+samm40me@users.noreply.github.com>
2021-11-08 11:42:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template  
 import flask
+from flask import Flask, render_template  
+import flask
 from flask import request, jsonify
 import requests
 from bs4 import BeautifulSoup as bs
@@ -19,17 +21,17 @@ app = flask.Flask(__name__)
 
 
 @app.route('/job', methods=['POST'])
-def post_request(): 
+def post_request(self, job): 
     return post_request()
 
 #    print("status code = ", post_request()) 
 
-@app.route('/jobs/V1.0/status', methods=['GET'])
-def crawler(): 
+@app.route('/jobs/<string:job-id>/status', methods=['GET'])
+def crawler(self, status): 
     return job_counter(SITES)
 
-@app.route('/jobs/V1.0/result', methods=['GET'])
-def getdata():
+@app.route('/jobs/<string:job-id>/result', methods=['GET'])
+def getdata(self, result):
     return getdata()
 
 if __name__ == '__main__':
